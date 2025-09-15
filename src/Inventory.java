3b3049caf8a65b5bcc8a7317fc3f29172718599d
bassanthossamm <110024214+bassanthossamm@users.noreply.github.com>
2025-09-15 18:44:17 +0300

    added file read and write

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,12 @@
-;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;;
+
 public class Inventory {
     static int numberOfBooks = 0;
     Book[] books = new Book[0];
@@ -17,9 +25,68 @@ public class Inventory {
         numberOfBooks++;
     }
 
+    public void addBook() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter book name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter author name: ");
+        String author = scanner.nextLine();
+        System.out.print("Enter release year: ");
+        int releaseYear = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter genre: ");
+        String genre = scanner.nextLine();
+        Book book = new Book(name, author, releaseYear, genre);
+        Book[] newBooks = new Book[numberOfBooks + 1];
+        for (int i = 0; i < books.length; i++) {
+            newBooks[i] = books[i];
+        }
+        newBooks[numberOfBooks] = book;
+        books = newBooks;
+        numberOfBooks++;
+        scanner.close();
+    }
+
     public void getBooks() {
         for (int i = 0; i < numberOfBooks; i++) {
             System.out.println((i + 1) + "-" + books[i].getName());
+        }
+    }
+
+    public void readBooksFromFile() {
+        String filepath = "C:\\Users\\Bassant\\Desktop\\javaLearning\\LibraryManagementSystem\\src\\Storage.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String name, author, genre;
+            int releaseYear;
+            while ((name = reader.readLine()) != null) {
+                author = reader.readLine();
+                releaseYear = Integer.parseInt(reader.readLine());
+                genre = reader.readLine();
+                Book book = new Book(name, author, releaseYear, genre);
+                addBook(book);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("could not locate file!");
+        } catch (IOException e) {
+            System.out.println("something went wrong!");
+        }
+    }
+
+    public void writeBooksToFile() {
+        String filepath = "C:\\Users\\Bassant\\Desktop\\javaLearning\\LibraryManagementSystem\\src\\Storage.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+            for (int i = 0; i < numberOfBooks; i++) {
+                writer.write(books[i].getName());
+                writer.newLine();
+                writer.write(books[i].getAuthor());
+                writer.newLine();
+                writer.write(String.valueOf(books[i].getReleaseYear()));
+                writer.newLine();
+                writer.write(books[i].getGenre());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("something went wrong!");
         }
     }
 }
--- src/LibraryAPP.java
+++ src/LibraryAPP.java
@@ -1,14 +1,13 @@
 public class LibraryAPP {
     public static void main(String[] args) throws Exception {
-        Book book2 = new Book("harry potter and the philosopher's stone", "j.k. rowling", 1997, "fantasy");
-        Book book3 = new Book("gone with the wind");
-        Book book4 = new Book("sherlock holmes");
         Inventory inventory = new Inventory();
-        inventory.addBook(book2);
-        inventory.addBook(book3);
-        inventory.addBook(book4);
-        inventory.getBooks();
+        inventory.readBooksFromFile();
         System.out.println(inventory.getNumberOfBooks());
-        System.out.println(book2.toString());
+        for (int i = 0; i < inventory.getNumberOfBooks(); i++) {
+            System.out.println((i + 1) + "-" + inventory.books[i].toString());
+        }
+        // inventory.addBook();
+        System.out.println(inventory.getNumberOfBooks());
+        inventory.writeBooksToFile();
     }
 }
